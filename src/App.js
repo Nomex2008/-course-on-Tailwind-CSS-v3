2e013868d54ca4ff3e6009073b4a77dc93ceda09
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-01 23:48:48 +0200

    Commit 2
    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <div className="">
-      <h1 className="m-10">
+    <div className="m-10 border border-purple-500 rounded-lg">
+      <h1 className="text-xl font-bold text-center text-gray-900 py-2 px-14">
         Hello world!
       </h1>
-      <button className="m-10">
+      <button className="block mx-auto mt-5 mb-10 bg-purple-600 py-2 px-14 rounded-xl hover:bg-opacity-60 transition-all delay-300 ease-linear">
         Click me!
       </button>
     </div>
