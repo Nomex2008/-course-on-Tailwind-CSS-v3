08908f0387e3cb9856be5766c145a12fd1ad1a66
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-02 23:54:51 +0200

    Commit 3
    Commit 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import styles from './app.module.scss'
+
 function App() {
   return (
-    <div className="m-10 border border-purple-500 rounded-lg">
-      <h1 className="text-xl font-bold text-center text-gray-900 py-2 px-14">
+    <div className={styles.parent}>
+      <h1 className={styles.title}>
         Hello world!
       </h1>
-      <button className="block mx-auto mt-5 mb-10 bg-purple-600 py-2 px-14 rounded-xl hover:bg-opacity-60 transition-all delay-300 ease-linear">
+      <button className={styles.btn}>
         Click me!
       </button>
     </div>
