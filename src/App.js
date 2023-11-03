6021063554a32c8556c843d3f1d76fe016186f75
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-03 18:25:18 +0200

    Commit 4
    Commit 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import styles from './app.module.scss'
 
 function App() {
+
+  const  [email,setEmail] = useState('')
+  const  [password,setPassword] = useState('')
+
   return (
+    <>
     <div className={styles.parent}>
       <h1 className={styles.title}>
         Hello world!
@@ -10,6 +16,33 @@ function App() {
         Click me!
       </button>
     </div>
+
+    <div className={styles.parent2}>
+      <h1 className={styles.title}>
+        Hello world!
+      </h1>
+
+      <input
+      className={styles.input}
+      placeholder='Email'
+      value={email}
+      onChange={e => setEmail(e.target.value)}
+      >
+      </input>
+
+      <input placeholder='Password'
+      className={styles.input}
+      value={password}
+      onChange={e => setPassword(e.target.value)}
+      >
+      </input>
+
+      <button className={styles.btn2}>
+        Click me!
+      </button>
+
+    </div>
+    </>
   );
 }
 
