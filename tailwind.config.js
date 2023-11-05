f1722b8f5ac62407527fd006240ab5ad775b8c91
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-05 20:12:25 +0200

    Commit 5
    Commit 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      //primary:'red',
+      transitionTimingFunction:{
+        DEFAULT:'ease-in-out',
+      },
+      transitionDuration:{
+        DEFAULT:'300ms',
+      }
+    },
   },
   plugins: [],
 }
